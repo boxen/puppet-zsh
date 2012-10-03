4aca834e4a327b036d8e7621c1a2b5b69e68b849
Will Farrington <wfarr@github.com>
2012-10-03 13:19:02 -0700

    TomDoc zsh

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,14 @@
+# Public: Install zsh into Homebrew and add to /etc/shells.
+#
+# Examples
+#
+#   include zsh
 class zsh {
   package { 'zsh': }
 
   file_line { 'add zsh to /etc/shells':
     path    => '/etc/shells',
-    line    => '/opt/github/homebrew/bin/zsh',
+    line    => "${boxen::config::homebrewdir}/bin/zsh",
     require => Package['zsh'],
   }
 }
